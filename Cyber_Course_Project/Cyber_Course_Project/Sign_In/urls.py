c21a41e05324b6581ea41e375698fb11d3671853
Michael <m.shot751@gmail.com>
2025-11-12 09:51:55 -0500

    Add SQL Injection demonstration feature
    - Added vulnerable SQL injection login view
    - Created Sign_In_Sql_Injection.html template with security warning (remove from Secured version upon creating  Vulnerable Project)
    - Updated Sign_In URLs to include /sql_injection/ endpoint (remove from Secured version upon creating  Vulnerable Project)
    - Implemented raw SQL query execution to demonstrate injection vulnerability (remove from Secured version upon creating Vulnerable Project|| For use in Vulnerable Project as Authentication)
    - Added SQL injection Log In button to  home.html template (remove from Secured version upon creating  Vulnerable Project)

--- Cyber_Course_Project/Cyber_Course_Project/Sign_In/urls.py
+++ Cyber_Course_Project/Cyber_Course_Project/Sign_In/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.Sign_In_view, name='Sign_In'),
+    
+    #remove path after making vulnerable version of the project
+    path('sql_injection/', views.Sql_Injection_login_view, name='Sql_Injection_Login'), 
 ]
